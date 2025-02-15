f467e105677818b944ca78d9064e37c36a1b3daa
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2025-02-15 12:33:45 -0800

    sidebar updates

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { HomeIcon, BuildingOfficeIcon, PlayIcon, ChartBarIcon } from '@heroicons/react/24/outline';
 
 export const Sidebar = () => {
   return (
-    <div className="w-16 bg-gray-900 h-screen flex flex-col items-center py-4">
-      <div className="text-white font-bold mb-8">GI</div>
+    <div className="w-16 bg-[#252527] h-screen flex flex-col items-center py-4">
+      <div className="mb-8">
+        <Image
+          src="/AlwaysScheming_RGB_mark_pinkgraphitebg (2).png"
+          alt="Always Scheming Logo"
+          width={48}
+          height={48}
+          className="w-12 h-12"
+        />
+      </div>
       <nav className="flex flex-col gap-6">
         <Link href="/" className="text-gray-400 hover:text-white">
           <HomeIcon className="w-6 h-6" />
